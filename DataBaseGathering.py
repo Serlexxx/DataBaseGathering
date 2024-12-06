84553ef1b9a7a22742b8ada75acc010a4d058444
Serlexxx <kocsharov.sereja@yandex.ru>
2024-12-06 15:36:47 +0300

    DataBaseGathering.py: fix command sql for initial db

--- DataBaseGathering.py
+++ DataBaseGathering.py
@@ -11,15 +11,15 @@ class DataBaseGathering(DataBase):
                 "company_id"	INTEGER NOT NULL UNIQUE,
                 "name"	        TEXT NOT NULL,
                 "company_hash"	TEXT NOT NULL UNIQUE,
-                PRIMARY         KEY("company_id" AUTOINCREMENT)
+                PRIMARY KEY("company_id" AUTOINCREMENT)
             );
         ''', ())
 
         self.execute('''
             CREATE TABLE IF NOT EXISTS "Gathering" (
                 "gathering_id"	INTEGER NOT NULL UNIQUE,
-                "date"	TEXT,
-                "location"	TEXT,
+                "date"	        TEXT NOT NULL,
+                "name"	        TEXT NOT NULL,
                 "company_id"	INTEGER NOT NULL,
                 FOREIGN KEY("company_id") REFERENCES "Company"("company_id"),
                 PRIMARY KEY("gathering_id")
@@ -28,9 +28,9 @@ class DataBaseGathering(DataBase):
 
         self.execute('''
             CREATE TABLE IF NOT EXISTS "Person" (
-                "person_id"	INTEGER NOT NULL UNIQUE,
-                "person_name"	TEXT,
-                "tg_tag"	TEXT,
+                "person_id"	    INTEGER NOT NULL UNIQUE,
+                "person_name"	TEXT NOT NULL,
+                "tg_tag"	    TEXT NOT NULL,
                 PRIMARY KEY("person_id" AUTOINCREMENT)
             );
         ''', ())
@@ -38,11 +38,11 @@ class DataBaseGathering(DataBase):
         self.execute('''
             CREATE TABLE IF NOT EXISTS "ReceiptPosition" (
                 "receipt_position_id"	INTEGER NOT NULL UNIQUE,
-                "description"	TEXT,
-                "amount"	INTEGER NOT NULL,
-                "gathering_id"	INTEGER NOT NULL,
-                "payed_person_id"	INTEGER NOT NULL,
-                "group_id"	INTEGER NOT NULL,
+                "description"	        TEXT,
+                "amount"	            INTEGER NOT NULL,
+                "gathering_id"	        INTEGER NOT NULL,
+                "payed_person_id"	    INTEGER NOT NULL,
+                "group_id"	            INTEGER NOT NULL,
                 FOREIGN KEY("payed_person_id") REFERENCES "Person",
                 PRIMARY KEY("receipt_position_id" AUTOINCREMENT)
             );
@@ -50,8 +50,9 @@ class DataBaseGathering(DataBase):
 
         self.execute('''
             CREATE TABLE IF NOT EXISTS "CompanyPerson" (
-                "company_id"	INTEGER NOT NULL,
-                "person_id"	INTEGER NOT NULL,
+                "company_id"    INTEGER NOT NULL,
+                "person_id"	    INTEGER NOT NULL,
+                PRIMARY KEY("person_id","company_id"),
                 FOREIGN KEY("person_id") REFERENCES "Person"("person_id"),
                 FOREIGN KEY("company_id") REFERENCES "Company"("company_id")
             );
@@ -60,7 +61,7 @@ class DataBaseGathering(DataBase):
         self.execute('''
             CREATE TABLE IF NOT EXISTS "PersonGroup" (
                 "group_id"	INTEGER NOT NULL UNIQUE,
-                "paid"	BLOB NOT NULL,
+                "paid"	    BLOB NOT NULL,
                 PRIMARY KEY("group_id" AUTOINCREMENT)
             );
         ''', ())
