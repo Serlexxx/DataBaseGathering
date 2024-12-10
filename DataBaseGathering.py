f39f12ed0ec249bd8b56fadcba50d75e54b1749d
Serlexxx <kocsharov.sereja@yandex.ru>
2024-12-10 21:59:04 +0300

    DataBaseGathering.py: review database structure
    A problem was discovered that the payment status belonged to a group of people, not to each person. In this table, we tried to fix, need refactoring of implemented functions

--- DataBaseGathering.py
+++ DataBaseGathering.py
@@ -139,16 +139,46 @@ class DataBaseGathering(DataBase):
                 "name"          TEXT NOT NULL,
                 "company_id"    INTEGER NOT NULL,
                 FOREIGN KEY("company_id") REFERENCES "Company"("company_id"),
-                PRIMARY KEY("gathering_id")
+                PRIMARY KEY("gathering_id" AUTOINCREMENT)
             );
         ''', ())
 
         self.execute('''
             CREATE TABLE IF NOT EXISTS "Person" (
                 "person_id"     INTEGER NOT NULL UNIQUE,
-                "person_name"   TEXT NOT NULL,
                 "tg_tag"        TEXT NOT NULL,
                 PRIMARY KEY("person_id" AUTOINCREMENT)
+            );
+        ''', ())
+
+        # Вспомогательная таблица
+        self.execute('''
+            CREATE TABLE IF NOT EXISTS "CompanyPerson" (
+                "company_id"    INTEGER NOT NULL UNIQUE,
+                "person_id"     INTEGER NOT NULL,
+                PRIMARY KEY("person_id", "company_id"),
+                FOREIGN KEY("person_id")    REFERENCES "Person"("person_id"),
+                FOREIGN KEY("company_id")   REFERENCES "Company"("company_id")
+            );
+        ''', ())
+
+        self.execute('''
+            CREATE TABLE IF NOT EXISTS "PersonGroup" (
+                "group_id"          INTEGER NOT NULL UNIQUE,
+                "count_person"      INTEGER NOT NULL,
+                PRIMARY KEY("group_id" AUTOINCREMENT)
+            );
+        ''', ())
+
+        self.execute('''
+            CREATE TABLE IF NOT EXISTS "PaymentStatus" (
+                "payment_status_id"     INTEGER NOT NULL UNIQUE,
+                "is_paid"               BLOB NOT NULL,
+                "person_id"             INTEGER NOT NULL,
+                "group_id"              INTEGER NOT NULL,
+                FOREIGN KEY("person_id")    REFERENCES "Person"("person_id"),
+                FOREIGN KEY("group_id")     REFERENCES "PersonGroup"("group_id"),
+                PRIMARY KEY("payment_status_id" AUTOINCREMENT)
             );
         ''', ())
 
@@ -159,36 +189,11 @@ class DataBaseGathering(DataBase):
                 "amount"                REAL NOT NULL,
                 "gathering_id"          INTEGER NOT NULL,
                 "payed_person_id"       INTEGER NOT NULL,
-                "group_id"              INTEGER NOT NULL,
-                FOREIGN KEY("payed_person_id") REFERENCES "Person",
+                "payment_status_id"     INTEGER NOT NULL,
+                FOREIGN KEY("payed_person_id") REFERENCES "Person"("person_id"),
+                FOREIGN KEY("payment_status_id") REFERENCES "PaymentStatus"("payment_status_id"),
                 PRIMARY KEY("receipt_position_id" AUTOINCREMENT)
             );
         ''', ())
 
-        self.execute('''
-            CREATE TABLE IF NOT EXISTS "CompanyPerson" (
-                "company_id"    INTEGER NOT NULL,
-                "person_id"     INTEGER NOT NULL,
-                PRIMARY KEY("person_id","company_id"),
-                FOREIGN KEY("person_id") REFERENCES "Person"("person_id"),
-                FOREIGN KEY("company_id") REFERENCES "Company"("company_id")
-            );
-        ''', ())
 
-        self.execute('''
-            CREATE TABLE IF NOT EXISTS "PersonGroup" (
-                "group_id"  INTEGER NOT NULL UNIQUE,
-                "paid"      BLOB NOT NULL,
-                PRIMARY KEY("group_id" AUTOINCREMENT)
-            );
-        ''', ())
-
-        self.execute('''
-            CREATE TABLE IF NOT EXISTS "PersonGroupPerson" (
-                "group_id"  INTEGER NOT NULL,
-                "person_id" INTEGER NOT NULL,
-                PRIMARY KEY("group_id","person_id"),
-                FOREIGN KEY("person_id") REFERENCES "Person"("person_id"),
-                FOREIGN KEY("group_id") REFERENCES "PersonGroup"("group_id")
-            );
-        ''', ())
